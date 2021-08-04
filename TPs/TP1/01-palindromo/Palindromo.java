89f2d4375ae15d8fe76c7bf284f92b24931a79ff
João Augusto dos Santos Silva <joao@MacBook-Air-de-Joao.local>
2021-08-04 20:07:02 -0300

    04/08

---
+++ TPs/TP1/01-palindromo/Palindromo.java
@@ -0,0 +1,28 @@
+class Palindromo{ 
+    public static boolean isFim(String s){
+        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static boolean isPalindromo(String s){
+
+
+    }
+
+    public static void main (String[] args){
+        String[] entrada = new String[1000];
+        int numEntrada = 0;
+
+        //Leitura da entrada padrao
+        do {
+            entrada[numEntrada] = MyIO.readLine();
+        } while (isFim(entrada[numEntrada++]) == false);
+        numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+        
+        for(int i = 0; i < numEntrada; i++){
+            if(isPalindromo(entrada[numEntrada]) == true)
+                MyIO.println("SIM");
+            else
+                MyIO.println("NAO");
+        }
+    }
+}
