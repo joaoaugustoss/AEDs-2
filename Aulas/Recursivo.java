8d1a29ca1b73264e682d8d09a08dc33a5b708518
joaoaugustoss <joao@MacBook-Air-de-Joao.local>
2021-08-10 13:33:27 -0300

    10-08

--- Aulas/Recursivo.java
+++ Aulas/Recursivo.java
@@ -7,15 +7,15 @@ class Recursivo{
         return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
     }
 
-    public static int contarLetrasMaiusculas (String s, int count){
+    public static int contarLetrasMaiusculas(String s, int count){
         int resp = 0;
-        MyIO.println("Início recursivo");
-        if(s.charAt(count) == '\n')
+        if(isFim(s) == true)
             resp = 0;
-        else if(isMaiuscula(s.charAt(count)) == true)
+        else if(isMaiuscula(s.charAt(count)) == true){
             resp++;
-        //contarLetrasMaiusculas(s, count++);
-        MyIO.println("Fim recursivo"); 
+            contarLetrasMaiusculas(s, count++);
+        } else
+            resp = contarLetrasMaiusculas(s, count++);
         return resp;
     }
 
@@ -26,7 +26,7 @@ class Recursivo{
         //Leitura da entrada padrao
         do {
             entrada[numEntrada] = MyIO.readLine();
-        } while (isFim(entrada[numEntrada++]) == false);
+        } while(contarLetrasMaiusculas(entrada[numEntrada++], 0) != 0);
         numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
 
         //Para cada linha de entrada, gerando uma de saida contendo o numero de letras maiusculas da entrada
--- TPs/TP1/03-Cesar/Cesar.java
+++ TPs/TP1/03-Cesar/Cesar.java
@@ -1,14 +1,29 @@
-class Cesar(){
+class Cesar{
+    public static boolean isFim(String s){
+        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
 
+    public static String converterLinha(String s){
+        String resp = "";
+        for(int i = 0; i < s.length(); i++){
+            resp = resp +(char)(s.charAt(i) + 3);
+        }
+        return resp;
+    }
 
-
-
-
-
-
-
-
-    static void main(String[] args){
+    public static void main (String[] args){
         String[] entrada = new String[1000];
+        int numEntrada = 0;
+
+        //Leitura da entrada padrao
+        do {
+            entrada[numEntrada] = MyIO.readLine();
+        } while (isFim(entrada[numEntrada++]) == false);
+        numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+
+        //Para cada linha de entrada, gerando uma de saida contendo o numero de letras maiusculas da entrada
+        for(int i = 0; i < numEntrada; i++){
+            MyIO.println(converterLinha(entrada[i]));
+        }
     }
 }
