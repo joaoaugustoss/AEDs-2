b105a0f50c270be889bae968e63253783399318a
João Augusto <cop.joaoaugusto@gmail.com>
2021-09-15 13:44:01 -0300

    TP02

--- TPs/TP2/TP02Q02 - Registro em C/TP02Q02.c
+++ TPs/TP2/TP02Q02 - Registro em C/TP02Q02.c
@@ -5,13 +5,13 @@
 #include <stdbool.h>
 
 typedef struct Serie{
-    char name[30];
-    char format[30];
-    char duration[40];
-    char country[30];
-    char language[10];
-    char broadcaster[30];
-    char streaming[30];
+    char name[100];
+    char format[100];
+    char duration[100];
+    char country[100];
+    char language[100];
+    char broadcaster[100];
+    char streaming[100];
     int seasons;
     int episodes;
 }Serie;
@@ -28,7 +28,6 @@ int justInt(char line[]){
 }
 //método para a remoção das tags lidas nas linhas
 char* removeTags(char s[]){
-    //printf("ENTRADA\t%s\n", s);
     char *resp = (char *)malloc(sizeof(strlen(s)));
     int i = 0, j = 0;
     while(i < strlen(s)){
@@ -45,7 +44,6 @@ char* removeTags(char s[]){
         i++;
     }
     resp[j-1] = '\0';
-    //printf("SAIDA\t%s\n%lu\n", resp, strlen(resp));
     return resp;
 }
 //método para tratar o nome do arquivo lido e retorná-lo sem caracteres especiais
@@ -65,80 +63,72 @@ char* getName(char fileName[]){
     return resp;
 }
 //método para printar os atributos da série
-void print(Serie *serie){
-    printf("%s %s %s %s %s %s %s %d %d\n", serie->name, serie->format, serie->duration, serie->country, serie->language, serie->broadcaster,
-    serie->streaming, serie->seasons, serie->episodes);
+void print(Serie serie){
+    printf("%s %s %s %s %s %s %s %d %d\n", serie.name, serie.format, serie.duration, serie.country, serie.language, serie.broadcaster,
+    serie.streaming, serie.seasons, serie.episodes);
 }
 //método para testar se a entrada == FIM para finalizar as entradas
 bool isFim(char s[]){
     return (strlen(s) == 3 && s[0] == 'F' && s[1] == 'I' && s[2] == 'M');
 }
 //método para leitura do arquivo html e tratamento do mesmo
-void read(char *fileName, Serie *serie){
-    char *line = (char *)malloc(1600 * sizeof(char));
-    char file[50], *teste1, *teste2;
+void read(char *fileName, Serie serie){
+    char *line = (char *)malloc(1500 * sizeof(char));
+    char file[50];
     sprintf(file, "/tmp/series/%s", fileName);
     FILE *fp = fopen(file, "r"); //abertura do arquivo para leitura
     if(fp == NULL){ //teste para verificar se foi possível abrir o arquivo corretamente
         printf("Erro ao abrir o arquivo!!!\n");
     }
+    
     //set nome da série
-    printf("%s", getName(fileName));
+    strcpy(serie.name, getName(fileName));
 
     //set formato da série
-    while(!strstr(fgets(line, 1000, fp), "Formato"));
-    fgets(line, 1000, fp);
-    printf(" %s", removeTags(line));
-    //strcpy(serie->format, removeTags(line));
+    while(!strstr(fgets(line, 1500, fp), "Formato"));
+    fgets(line, 1500, fp);
+    strcpy(serie.format, removeTags(line));
 
     //set duração da série
-    while(!strstr(fgets(line, 1000, fp), "Duração"));
-    fgets(line, 1000, fp);
-    printf(" %s", removeTags(line));
-    //strcpy(removeTags(line), serie->format);
+    while(!strstr(fgets(line, 1500, fp), "Duração"));
+    fgets(line, 1500, fp);
+    strcpy(serie.duration, removeTags(line));
 
     //set país da série
-    while(!strstr(fgets(line, 1600, fp), "País de origem"));
-    fgets(line, 1600, fp);
-    printf(" %s", removeTags(line));
-    //strcpy(removeTags(line), serie->format);
+    while(!strstr(fgets(line, 1500, fp), "País de origem"));
+    fgets(line, 1500, fp);
+    strcpy(serie.country, removeTags(line));
 
     //set idioma da série
-    while(!strstr(fgets(line, 1000, fp), "Idioma original"));
-    fgets(line, 1000, fp);
-    printf(" %s", removeTags(line));
-    //strcpy(removeTags(line), serie->format);
+    while(!strstr(fgets(line, 1500, fp), "Idioma original"));
+    fgets(line, 1500, fp);
+    strcpy(serie.language, removeTags(line));
 
     //set emissora da série
-    while(!strstr(fgets(line, 1000, fp), "Emissora de televisão"));
-    fgets(line, 1000, fp);
-    printf(" %s", removeTags(line));
-    //strcpy(removeTags(line), serie->format);
+    while(!strstr(fgets(line, 1500, fp), "Emissora de televisão"));
+    fgets(line, 1500, fp);
+    strcpy(serie.broadcaster, removeTags(line));
 
     //set transmissão original da série
-    while(!strstr(fgets(line, 1600, fp), "Transmissão original"));
-    fgets(line, 1600, fp);
-    printf(" %s", removeTags(line));
-    //strcpy(removeTags(line), serie->format);
+    while(!strstr(fgets(line, 1500, fp), "Transmissão original"));
+    fgets(line, 1500, fp);
+    strcpy(serie.streaming, removeTags(line));
 
     //set temporadas da série
-    while(!strstr(fgets(line, 1000, fp), "N.º de temporadas"));
-    fgets(line, 1000, fp);
-    printf(" %d", justInt(removeTags(line)));
-    //strcpy(removeTags(line), serie->format);
+    while(!strstr(fgets(line, 1500, fp), "N.º de temporadas"));
+    fgets(line, 1500, fp);
+    serie.seasons = justInt(removeTags(line));
 
     //set episódios da série
-    while(!strstr(fgets(line, 1000, fp), "N.º de episódios"));
-    fgets(line, 1000, fp);
-    printf(" %d\n", justInt(removeTags(line)));
-    //strcpy(removeTags(line), serie->format);
+    while(!strstr(fgets(line, 1500, fp), "N.º de episódios"));
+    fgets(line, 1500, fp);
+    serie.episodes = justInt(removeTags(line));
 
-    //print(serie);
+    print(serie);
     fclose(fp);
 }
 
 int main(){
-    Serie *serie;
     char entrada[1000][100];
     int numEntrada = 0;
 
@@ -146,10 +136,10 @@ int main(){
         scanf(" %[^\n]s", entrada[numEntrada]);
     } while(isFim(entrada[numEntrada++]) == false);
     numEntrada--;
+    Serie serie[numEntrada];
 
     for(int i = 0; i < numEntrada; i++){
-        read(entrada[i], serie);
-        //printf("%s\n", serie->format);
+        read(entrada[i], serie[i]);
     }
     
     return 0;
---
+++ TPs/TP2/TP02Q03 - Pesquisa Sequencial em Java/TP02Q03.java
@@ -0,0 +1,298 @@
+import java.io.*;
+import java.util.*;
+import java.io.FileReader;
+
+class Serie{
+    //declaração dos atributos
+    private String name;
+    private String format;
+    private String duration;
+    private String country;
+    private String language;
+    private String broadcaster;
+    private String streaming;
+    private int seasons;
+    private int episodes;
+    //construtor primário
+    public Serie(){
+        name = "";
+        format = "";
+        duration = "";
+        country = "";
+        language = "";
+        broadcaster = "";
+        streaming = "";
+        seasons = 0;
+        episodes = 0;
+    }
+    //construtor secundário
+    public Serie(String name, String format, String duration, String country, String language, String broadcaster, String streaming, int seasons, 
+    int episodes){
+        this.name = name;
+        this.format = format;
+        this.duration = duration;
+        this.country = country;
+        this.language = language;
+        this.broadcaster = broadcaster;
+        this.streaming = streaming;
+        this.seasons = seasons;
+        this.episodes = episodes;
+    }
+    //método para setar o atributo name
+    public void setName(String name){
+        this.name = name;
+    }
+    //método para setar o atributo formato
+    public void setFormat(String format){
+        this.format = format;
+    }
+    //método para setar o atributo duration
+    public void setDuration(String duration){
+        this.duration = duration;
+    }
+    //método para setar o atributo country
+    public void setCountry(String country){
+        this.country = country;
+    }
+    //método para setar o atributo language
+    public void setLanguage(String language){
+        this.language = language;
+    }
+    //método para setar o atributo broadcaster
+    public void setBroadcaster(String broadcaster){
+        this.broadcaster = broadcaster;
+    }
+    //método para setar o atributo streaming
+    public void setStreaming(String streaming){
+        this.streaming = streaming;
+    }
+    //método para setar o atributo seasons
+    public void setSeasons(int seasons){
+        this.seasons = seasons;
+    }
+    //método para setar o atributo episodes
+    public void setEpisodes(int episodes){
+        this.episodes = episodes;
+    }
+    //método para retornar o atributo name
+    public String getName(){ 
+        return this.name; 
+    }
+    //método para retornar o atributo format
+    public String getFormat(){ 
+        return this.format; 
+    }
+    //método para retornar o atributo duration
+    public String getDuration(){ 
+        return this.duration; 
+    }
+    //método para retornar o atributo country
+    public String getCountry(){ 
+        return this.country; 
+    }
+    //método para retornar o atributo language
+    public String getLanguage(){ 
+        return this.language; 
+    }
+    //método para retornar o atributo broadcaster
+    public String getBroadcaster(){ 
+        return this.broadcaster; 
+    }
+    //método para retornar o atributo streaming
+    public String getStreaming(){ 
+        return this.streaming; 
+    }
+    //método para retornar o atributo seasons
+    public int getSeasons(){ 
+        return this.seasons; 
+    }
+    //método para retornar o atributo episodes
+    public int getEpisodes(){ 
+        return this.episodes; 
+    }
+    //método para clonar a classe
+    public Serie clone(){
+        Serie resp = new Serie();
+        resp.name = this.name;
+        return resp;
+    }
+    //método para printar a classe
+    public void printClass(){
+        System.out.println(this.name + " " + this.format + " " + this.duration + " " + this.country + " " + this.language + " " + this.broadcaster + " " +
+        this.streaming + " " + this.seasons + " " + this.episodes);
+    }
+    //método para tratar a linha, deixar apenas números e converter o retorno de String para Integer
+    public int justInt(String line){
+        String resp = "";
+        for(int i = 0; i < line.length(); i++){
+            if(line.charAt(i) >= '0' && line.charAt(i) <= '9'){ //caso o caracter seja um número ele é concatenado a variável resp
+                resp += line.charAt(i);
+            } else { //caso seja outro caracter, o i recebe o valor da condição de parada e o método de repetição é encerrado
+                i = line.length();
+            }
+        }
+        return Integer.parseInt(resp); //conversão da string resp para número inteiro a ser retornado
+    }
+    //método para a remoção das tags da linha lida do arquivo para retornar apenas o que é desejado
+    public String removeTags(String line){
+        String resp = "";
+        int i = 0;
+        while(i < line.length()){ //enquanto i for menor que o tamanho da String linha
+            if(line.charAt(i) == '<'){ // é testado para verificar se o contador i ainda está dentro das tags
+                i++;
+                while(line.charAt(i) != '>') i++; //ao encontrar o sinal de fechamento das tags o laço de repetição é encerrado
+            } else if(line.charAt(i) == '&'){ //mesmo tratamento de cima mas para outras exceções presentes em alguns outros arquivos
+                i++;
+                while(line.charAt(i) != ';') i++;
+            } else { //o que estiver fora das tags é concatenado a String resp a ser retornada
+                resp += line.charAt(i);
+            }
+            i++;
+        }
+        //System.out.println(resp);
+        return resp;
+    }
+    //método para tratar o nome do arquivo e retornar o nome da série
+    public String searchName(String fileName){
+        String resp = "";
+        for(int i = 0; i < fileName.length(); i++){
+            if(fileName.charAt(i)  == '_'){ //caso o caracter na posição i seja igual ao '_' a variável resp recebe um espaço em branco
+                resp += ' ';
+            } else { //caso não tenha espaço em branco o caracter é concatenado à string resp
+                resp += fileName.charAt(i);
+            }
+        }
+        return resp.substring(0, resp.length()-5); //retorno da substring resp retirando os 5 últimos caracteres relacionados à extensão do arquivo
+    }
+    //método para leitura do arquivo .html e tratamento das linhas
+    public void readClass(String fileName){
+        String line;
+        String resp = "";
+        String file = "/tmp/series/" + fileName;
+        try {
+            FileReader fileReader = new FileReader(file); //declaração da variável fileReader que será recebida pelo bufferedReader
+
+            BufferedReader br = new BufferedReader(fileReader); //declaração do bufferedReader para leitura do arquivo
+            
+            //set nome da série
+            this.name = searchName(fileName);
+            
+            //set Formato da série
+            while(!br.readLine().contains("Formato"));
+            this.format = removeTags(br.readLine());
+
+            //set duração da série
+            while(!br.readLine().contains("Duração"));
+            this.duration = removeTags(br.readLine());
+
+            //set país da série
+            while(!br.readLine().contains("País de origem"));
+            this.country = removeTags(br.readLine());
+
+            //set idioma da série
+            while(!br.readLine().contains("Idioma original"));
+            this.language = removeTags(br.readLine());
+
+            //set emissora da série
+            while(!br.readLine().contains("Emissora de televisão"));
+            this.broadcaster = removeTags(br.readLine());
+
+            //set transmissão original da série
+            while(!br.readLine().contains("Transmissão original"));
+            this.streaming = removeTags(br.readLine());
+
+            //set temporadas da série
+            while(!br.readLine().contains("N.º de temporadas"));
+            this.seasons = justInt(removeTags(br.readLine()));
+
+            //set episódios da série
+            while(!br.readLine().contains("N.º de episódios"));
+            this.episodes = justInt(removeTags(br.readLine()));
+            
+            //fechamento do bufferedReader
+            br.close();         
+        //Tratamento de exceções
+        } catch(FileNotFoundException e) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        } catch(IOException e) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+    }
+}    
+
+//classe main
+public class TP02Q03{
+    static int count = 0;
+    public static void main(String[] args){
+        //Geracao teste;
+        long inicio = now();
+        String[] entrada = new String[1000];
+        String[] pesquisa = new String[1000];
+        int numEntrada = 0, numEntrada2 = 0;
+
+        do{
+            entrada[numEntrada] = MyIO.readLine();
+            count++;
+        }while(entrada[numEntrada++].equals("FIM") == false);
+        numEntrada--;
+
+        do{
+            pesquisa[numEntrada2] = MyIO.readLine();
+            count++;
+        }while(pesquisa[numEntrada2++].equals("FIM") == false);
+        numEntrada2--;
+
+        Serie[] serie = new Serie[numEntrada];
+        
+        for(int i = 0; i < numEntrada; i++){
+            count++;
+            //System.out.println(entrada[i]);
+            serie[i] = new Serie();
+            serie[i].readClass(entrada[i]);
+        }
+
+        for(int i = 0; i < numEntrada2; i++){
+            count++;
+            if(pesqSeq(serie, pesquisa[i])){
+                System.out.println("SIM");
+            } else {
+                System.out.println("NÃO");
+            }
+        }
+        long fim = now();
+        saveFile((fim - inicio)/1000.0);
+    }
+
+    public static void saveFile(double time){
+        try{
+            FileWriter fileWriter = new FileWriter("724667_sequencial.txt");
+            BufferedWriter bw = new BufferedWriter(fileWriter); 
+            bw.write("Tempo de execução: " + time + "s");
+            bw.newLine();
+            bw.write("Número de comparações: " + count);
+            bw.close();
+        //Tratamento de exceções
+        } catch(FileNotFoundException e) {
+            System.out.println("Unable to open file '" + "724667_sequencial.txt" + "'");                
+        } catch(IOException e) {
+            System.out.println("Error reading file '" + "724667_sequencial.txt" + "'");
+        }
+    }
+
+    public static long now(){
+        return new Date().getTime();
+    }
+    //método para relizar a pesquisa sequencial dentro do vetor de séries
+    public static boolean pesqSeq(Serie[] serie, String entrada){
+        boolean resp = false;
+        int n = serie.length;
+        for(int i = 0; i < n; i++){
+            count++;
+            if(entrada.equals(serie[i].getName())){
+                resp = true;
+                i = n;
+            }
+        }
+        return resp;
+   }
+}
